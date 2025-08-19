89b69dd158631726bc6a2213fbd6982bc1ad00f1
princegupta1614 <princegupta7698@gmail.com>
2025-08-19 09:04:20 +0530

    Misllenious changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,4 +1,4 @@
-// import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
@@ -6,13 +6,13 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    // server: {
-    //   proxy: {
-    //     '/api': {
-    //       target: env.VITE_BACKEND_URL,
-    //       changeOrigin: true,
-    //     },
-    //   },
-    // },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
+      },
+    },
   }
 })
